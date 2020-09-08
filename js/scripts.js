11bf5593a76d9883fe4d4f34fa56321174fde520
bryaleman <54127678+bryaleman@users.noreply.github.com>
2020-09-08 09:11:24 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,11 +29,11 @@ esri = L.tileLayer.provider("Esri.WorldImagery").addTo(map);
 osm = L.tileLayer.provider("OpenStreetMap.Mapnik").addTo(map);
 
 // Indice de Vegetacion diciembre 2019
-var ndvi = L.imageOverlay("ndvi.png", 
-	[[10.1387257937176312, -84.3679128707571806], 
-	[9.9967152737829750, -84.2313861376834154]], 
-	{opacity:0.8}
-).addTo(map);
+///var ndvi = L.imageOverlay("ndvi.png", 
+///	[[10.1387257937176312, -84.3679128707571806], 
+///	[9.9967152737829750, -84.2313861376834154]], 
+///	{opacity:0.8}
+///).addTo(map);
 
 
 // Conjunto de control de Capas Base
@@ -43,35 +43,37 @@ var baseMaps = {
 };
 
 //Conjunto de capas overlay
-var overlayMaps = {
-	"NDVI Diciembre 2018": ndvi
-};
+///var overlayMaps = {
+///	"NDVI Diciembre 2018": ndvi
+///};
 
 // Fincas de CoopeVictoria
-$.getJSON("lotes_coopevictoriarl.geojson", function(geodata) {
-	var layer_geojson_lotes_coopevictoriarl = L.geoJson(geodata, {
-		style: function(feature) {
-			return {'color': "#c71d1a", 'weight': 1, 'fillOpacity': 0.0}
-		},
-		onEachFeature: function(feature, layer) {
-			var popupText = "Finca: " + feature.properties.FINCA + "<br>" + "Lote: " + feature.properties.LOTE +
-			"<br>" + "Variedad: " + feature.properties.VARIEDAD + "<br>" + "Área: " + feature.properties.AREA;
-			layer.bindPopup(popupText);
-		}			
-	}).addTo(map);
-	control_layers.addOverlay(layer_geojson_lotes_coopevictoriarl, 'Plantaciones de Caña CoopeVictoria R.L.');
-});
+///$.getJSON("lotes_coopevictoriarl.geojson", function(geodata) {
+///	var layer_geojson_lotes_coopevictoriarl = L.geoJson(geodata, {
+///		style: function(feature) {
+///			return {'color': "#c71d1a", 'weight': 1, 'fillOpacity': 0.0}
+///		},
+///		onEachFeature: function(feature, layer) {
+///			var popupText = "Finca: " + feature.properties.FINCA + "<br>" + "Lote: " + feature.properties.LOTE +
+///			"<br>" + "Variedad: " + feature.properties.VARIEDAD + "<br>" + "Área: " + feature.properties.AREA;
+///			layer.bindPopup(popupText);
+///		}			
+///	}).addTo(map);
+///	control_layers.addOverlay(layer_geojson_lotes_coopevictoriarl, 'Plantaciones de Caña CoopeVictoria R.L.');
+///});
 
 
 // Distritos de Influencia
-$.getJSON("distritos_influencia.geojson", function(geodata) {
+$.getJSON("2010_regiones_crtm05_reh.geojson", function(geodata) {
 	var layer_geojson_distritos_influencia = L.geoJson(geodata, {
 		style: function(feature) {
 			return {'color': "#000000", 'weight': 1, 'fillOpacity': 0.0}
 		},
 		onEachFeature: function(feature, layer) {
-			var popupText = "Provincia: " + feature.properties.provincia + "<br>" + "Cantón: " + feature.properties.canton +
-			"<br>" + "Distrito: " + feature.properties.distrito + "<br>" + "Área: " + feature.properties.area;
+			var popupText = "Cantón: " + feature.properties.ncanton + "pH Alto: " + feature.properties.ph_alto + "pH Medio: " + feature.properties.ph_medio +
+			    "pH Bajo: " + feature.properties.ph_bajo + "<br>"
+			 + "Ca Alto: " + feature.properties.ca_alto + "Ca Medio: " + feature.properties.ca_medio + "Ca Bajo: " + feature.properties.ca_bajo +"<br>" 
+			+ "Área: " + feature.properties.area;
 			layer.bindPopup(popupText);
 		}			
 	}).addTo(map);
